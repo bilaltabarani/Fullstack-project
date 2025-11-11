fc0abe395f4062ec05a6e4135c22f1184115300d
Bilal Tabarani <tabaranibilal23@gmail.com>
2025-11-11 10:54:53 +0100

    Basis van het project aangemaakt.

---
+++ script.js
@@ -0,0 +1,11 @@
+const testDiv = document.getElementById("test-div-js");
+
+testDiv.addEventListener("click", function () {
+    console.log("Hello World!");
+});
+
+const nav = document.getElementById("nav-js");
+
+nav.addEventListener("click", function () {
+    console.log("Nav werkt!");
+});
