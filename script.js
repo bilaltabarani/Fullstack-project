ec98e40873c4e01bd33bb4c7eaf762c3fb8c6362
Bilal Tabarani <tabaranibilal23@gmail.com>
2025-11-11 11:16:59 +0100

    Titel veranderd en een kleine beschrijving toegevoegd

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ testDiv.addEventListener("click", function () {
     console.log("Hello World!");
 });
 
+
 const nav = document.getElementById("nav-js");
 
 nav.addEventListener("click", function () {
