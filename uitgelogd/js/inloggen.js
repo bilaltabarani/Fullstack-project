9b45e7c5740e4bb3c0a399a77ed45166f4ef240c
Bilal Tabarani <tabaranibilal23@gmail.com>
2026-01-22 11:21:31 +0100

    filepaths aangepast + redundant files verwijderd

--- uitgelogd/js/inloggen.js
+++
@@ -1,7 +0,0 @@
-const inloggen_button = document.getElementById("inloggen-button-js");
-
-inloggen_button.addEventListener("click", function () {
-    console.log("inlogknop werkt!");
-    window.location.href='/ingelogd/html/home-ingelogd.html';
-})
-
